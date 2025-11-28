88415cb5044a78054f52cf2848cca3ea01116cb4
Yurii1960 <jura.ntu.most@gmail.com>
2025-11-28 20:26:29 +0200

    change

--- main67.py
+++
@@ -1,6 +0,0 @@
-a=1
-b=[a]
-print(b)
-c=2
-b.append(c)
-print(b)
--- main699.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime,date,timedelta
-todays=datetime.now()
-print(date.today())
-birthday='25.11.2020'
-data_birthday=datetime.strptime (birthday, '%Y.%m.%d').date()
-print(data_birthday)
